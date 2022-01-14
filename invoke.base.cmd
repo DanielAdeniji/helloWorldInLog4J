8173eb999f5dc34000055ea64f25bb7b8d018542
Daniel Adenji <daniel_adeniji@hotmail.com>
2022-01-14 05:01:41 -0800

    Adding back log4J Jar files

---
+++ invoke.base.cmd
@@ -0,0 +1,11 @@
+set "jar_log4j_folder=lib/log4j/%jar_log4j_version%"
+
+set "jar_log4j_explicit=%jar_log4j_folder%/*"
+
+set "jar_log4j_implicit=%jar_log4j_folder%/log4j-api-%jar_log4j_version%.jar;%jar_log4j_folder%/log4j-core-%jar_log4j_version%.jar;%jar_log4j_folder%/log4j-1.2-api-%jar_log4j_version%.jar"
+
+set "jar_log4j=%jar_log4j_implicit%"
+
+rem javac -cp .;%jar_log4j% helloLog4j.java
+
+java -cp .;%jar_log4j% helloLog4j
